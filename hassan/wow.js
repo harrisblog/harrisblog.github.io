aca6ef7f06ceede536fed8073a929d048c1b30e0
hassanblog <1069171387@qq.com>
2021-04-29 01:45:56 +0800

    Site updated: 2021-04-29 01:45:36

---
+++ hassan/wow.js
@@ -0,0 +1,51 @@
+// 首页文章卡片
+var arr = document.getElementsByClassName("recent-post-item");
+for (var i = 0; i < arr.length; i++) {
+    arr[i].classList.add("wow"); //必要项，添加wow.js标记
+    arr[i].classList.add("animate__zoomIn"); //必要项，添加样式动画
+}
+// 侧栏卡片
+var arr = document.getElementsByClassName("card-widget");
+for (var i = 0; i < arr.length; i++) {
+    arr[i].classList.add("wow");
+    arr[i].classList.add("animate__slideInRight");
+}
+// 友链 flexcard
+var arr = document.getElementsByClassName("flink-list-card");
+for (var i = 0; i < arr.length; i++) {
+    arr[i].classList.add("wow");
+    arr[i].classList.add("animate__flipInY");
+}
+// 友链 Volantis
+var arr = document.getElementsByClassName("site-card");
+for (var i = 0; i < arr.length; i++) {
+    arr[i].classList.add("wow");
+    arr[i].classList.add("animate__flipInY");
+}
+// 朋友圈
+var arr = document.getElementsByClassName("article-sort-item");
+for (var i = 0; i < arr.length; i++) {
+    arr[i].classList.add("wow"); //必要项，添加wow.js标记
+    arr[i].classList.add("animate__zoomIn"); //必要项，添加样式动画
+}
+// 公告栏
+var arr = document.getElementsByClassName("notice");
+for (var i = 0; i < arr.length; i++) {
+    arr[i].classList.add("wow"); //必要项，添加wow.js标记
+    arr[i].classList.add("animate__zoomIn"); //必要项，添加样式动画
+}
+//初始化函数
+wow = new WOW({
+    boxClass: 'wow',
+    // 当用户滚动时显示隐藏框的类名称
+    animateClass: 'animate__animated',
+    // 触发 CSS 动画的类名称（动画库默认为"animate.css"库）
+    offset: 0,
+    // 定义浏览器视口底部与隐藏框顶部之间的距离。
+    // 当用户滚动并到达此距离时，将显示隐藏的框。
+    mobile: false,
+    // 在移动设备上打开/关闭wow.js。
+    live: true
+    // 在页面上检查新的 wow.js元素。
+})
+wow.init();
